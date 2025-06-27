5cec3dc63300251cb9aa950356d2e4bd7068d656
krishnaluharuka6 <krishna.luharuka11@gmail.com>
2025-06-27 18:56:20 +0545

    Worked on JavaScript, added audio play feature

--- spotify/main.js
+++ spotify/main.js
@@ -1,13 +1,55 @@
 console.log("let's do it!");
 
-async function main(){
-    let a = await fetch("http://localhost:5500/spotify/images/songs");
+async function getSongs(){
+    let a = await fetch("images/songs/");
     let response = await a.text();
-    console.log(response);
+    // console.log(response);
+
     let div = document.createElement("div")
     div.innerHTML = response;
-    let tds = div.getElementsByTagName('td')
-    console.log(tds)
+
+    let as= div.getElementsByTagName("a");
+    let songs = []
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/songs/")[1])
+        }  
+    }
+    return songs
 }
+
+async function main(){
+    let songs = await getSongs();
+    console.log(songs); //list of songs
+
+    let songOL = document.querySelector(".songList").getElementsByTagName("ol")[0];
+
+    for (const song of songs) {
+        songOL.innerHTML = songOL.innerHTML + `<li>
+                                <div class="left">
+                                    <i class="bi bi-music-note-beamed"></i>
+                                    <div class="info">
+                                        <div>${song.replaceAll("%20"," ")}</div>
+                                        <div>Artist</div>
+                                    </div>
+                                </div>
+                                <i class="bi bi-play-circle"></i>
+                            </li>`;
+    }
+
+    //play first song
+    var audio = new Audio(songs[0]);
+
+    document.addEventListener("click",() => {
+        audio.play();
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
+    });
+
+    // audio.play();
+
+
+} 
 
 main()
